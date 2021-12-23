922420dcf20b7e3335b45f5ffe632651186ba880
Michael Hurley <qvantitative@gmail.com>
2021-12-23 18:58:10 -0500

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import '../styles/App.css';
-import '../components/style.css'
 import React from 'react';
 //import Navbar from '../components/Navbar';
 import { ChakraProvider, Image, extendTheme } from '@chakra-ui/react';
 //import Router from './Router';
-
 
 const theme = extendTheme({
   config: {
@@ -14,22 +12,15 @@ const theme = extendTheme({
 
 function MyApp({ Component, pageProps }) {
   return (
-  <ChakraProvider theme={theme}>
-    <div>
-    <header className='bg-blue'>
-    </header>
+    <ChakraProvider theme={theme}>
       <Image
-          src='https://ipfs.io/ipfs/QmfZsKy3u1yDAUAgaXqCcrkkP3up2iZorap7PUkZdGF2hu?filename=grillz-banner-yellow.jpg'
-        />
+            src='https://ipfs.io/ipfs/QmfZsKy3u1yDAUAgaXqCcrkkP3up2iZorap7PUkZdGF2hu?filename=grillz-banner-yellow.jpg'
+          />
       <Component {...pageProps} />
       <Image
-          src="https://ipfs.io/ipfs/QmYpvX7zfbyqZokKZekGsDsekK1rYVFjMcXfBUtea8h85o?filename=grillz-grid-right.jpg"
-        />
-      <Image 
-          src="https://ipfs.io/ipfs/QmP9K81VsuGvfjYsTRxW9FGZGnd6PuGdNh3VKC9TnzHJYt?filename=grillz-grid-left.jpg"
-        />
-    </div>
-  </ChakraProvider>
+            src="https://ipfs.io/ipfs/QmYpvX7zfbyqZokKZekGsDsekK1rYVFjMcXfBUtea8h85o?filename=grillz-grid-right.jpg"
+          />
+    </ChakraProvider>
   )
 };
 
