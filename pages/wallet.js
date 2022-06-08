c688f8bd696ec6fc1dce15a77d83932c153c43a8
qvantitative <qvantitative@gmail.com>
2022-06-08 06:10:28 -0400

    init

--- pages/wallet.js
+++ pages/wallet.js
@@ -333,41 +333,10 @@ export default function App() {
 }
 
 export async function getServerSideProps() {
-  const {ethereum} = window;
+  const res = await fetch('https://api.opensea.io/api/v1/collection');
+  const data = await res.json()
 
-  if (ethereum) {
-
-    try {
-      const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
-
-      const baseURL2 = "https://api.opensea.io/api/v1/collections"
-      const baseURL3 = "https://api.opensea.io/api/v1/collection"
-
-      const fetchURL = `${baseURL2}?asset_owner=${accounts[0]}&offset=0&limit=300`;
-      fetch(fetchURL, {
-          method: 'GET',
-          redirect: 'follow',
-        })
-          .then(resp => resp.json())
-          .then(data => {
-            const responses = data.map((data) =>
-                fetch(`${baseURL3}/${data.slug}`)
-                    .then((res) => res.json()),
-            );
-            Promise.all(responses)
-
-      return {
-        props: data
-      }
-
-      })
-
-      } catch (err) {
-        console.log(err);
-      }
-
-    }
-
+  return {
+    props: data,
+  };
 }
