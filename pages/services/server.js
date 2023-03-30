ec173da7481194bcbc8cfcb3f17572c279fe64e0
qvantitative <qvantitative@gmail.com>
2023-03-30 12:53:19 -0400

    init

--- pages/services/server.js
+++ pages/services/server.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 
-export const app = initializeApp({
+export default function app() {
+  return <>{initializeApp({
   apiKey: "AIzaSyC4l2w7fa3iTwmHszof6wsmRLoDqDw0KcU",
   authDomain: "qvantitative-77a18.firebaseapp.com",
   projectId: "qvantitative-77a18",
@@ -8,5 +9,6 @@ export const app = initializeApp({
   messagingSenderId: "622637274894",
   appId: "1:622637274894:web:f74edddd827aa0b1f2295f",
   measurementId: "G-SDB5M7VY1T"
-});
+})}</>;
+}
 
