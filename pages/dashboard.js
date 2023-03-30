a2bc86aab807ca5921c4e1e8b9be6792e29c75c5
qvantitative <qvantitative@gmail.com>
2023-03-29 23:39:48 -0400

    init

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -77,7 +77,7 @@ export default function Dashboard() {
                               </div>
                           </div>
                         :
-                        <Wallet/> || <WalletMB/>
+                        <Wallet/>
                   }
               </div>
               {/*
